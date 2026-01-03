fb2d21d86957b8a06f2916fae28f9feee412fc5a
maxykoin <stayerim@gmail.com>
2026-01-03 17:47:05 -0300

    feat: first upload. need to change some texts

---
+++ src/data/projects.js
@@ -0,0 +1,29 @@
+export const projects = [
+  {
+    id: "pokedex",
+    title: "Pokedex_V1.exe",
+    icon: "/icons/pokeball.png",
+    description: "A wild API project appeared! Built with React and PokeAPI.",
+    tech: ["React", "API", "CSS"],
+    github: "https://github.com/cunhanina/YOUR_REPO_NAME",
+    theme: "pokemon" // Special theme trigger
+  },
+  {
+    id: "portfolio",
+    title: "My_Portfolio.html",
+    icon: "/icons/computer.png",
+    description: "Recursion! The code for this very OS.",
+    tech: ["Astro", "98.css"],
+    github: "https://github.com/cunhanina/YOUR_REPO_NAME",
+    theme: "win95"
+  },
+  {
+    id: "todo",
+    title: "Task_Manager_98",
+    icon: "/icons/notepad.png",
+    description: "Keep track of your missions.",
+    tech: ["JavaScript", "Local Storage"],
+    github: "https://github.com/cunhanina/YOUR_REPO_NAME",
+    theme: "myspace"
+  }
+];
